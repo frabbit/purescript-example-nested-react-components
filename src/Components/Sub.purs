f9d3d8bb38819504395634be203ff0f8a7e96c4e
frabbit <frabbit77@googlemail.com>
2022-07-07 15:54:50 +0200

    add sub component and render it in root component

--- src/Components/Sub.purs
+++ src/Components/Sub.purs
@@ -1,13 +1,11 @@
-module Components.Root where
+module Components.Sub where
 
 import Prelude
 
-import Components.Sub as Sub
 import React.Basic.DOM as R
 import React.Basic.Hooks as H
 
 component :: H.Component Unit
 component = do
-  subComp <- Sub.component
   H.component "WritePanel" \_ -> React.do
-    pure $ R.div_ [R.text "Hello World", subComp unit]
+    pure $ R.div_ [R.text "Hello I'm a Sub-Component"]
